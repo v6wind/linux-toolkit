d0d9af0b4db5d7497048585366d889a8af66597d
v6wind <35835096+v6wind@users.noreply.github.com>
2025-08-25 13:21:09 +0800

    Update debian.sh

--- system-upgrade/debian.sh
+++ system-upgrade/debian.sh
@@ -44,6 +44,15 @@ __do_debian12_upgrade(){
     echo "[INFO] Please reboot"
 }
 
+__do_debian13_upgrade(){
+    echo "[INFO] Doing debian 13 upgrade..."
+    __do_apt_upgrade
+    sed -i 's/bookworm/trixie/g' /etc/apt/sources.list
+    sed -i 's/bookworm/trixie/g' /etc/apt/sources.list.d/*.list
+    sed -i 's/bookworm/trixie/g' /etc/apt/sources.list.d/*.sources
+    __do_apt_upgrade
+    echo "[INFO] Please reboot"
+}
 
 echo $RELEASE | grep ' 9 '
 if [ $? -eq 0 ]; then
@@ -60,5 +69,11 @@ fi;
 echo $RELEASE | grep ' 11 '
 if [ $? -eq 0 ]; then
     __do_debian12_upgrade
+    exit 0
+fi;
+
+echo $RELEASE | grep ' 12 '
+if [ $? -eq 0 ]; then
+    __do_debian13_upgrade
     exit 0
 fi;
